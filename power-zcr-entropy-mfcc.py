73a334baa9057105a3c7cd5be040d28eb7baae09
Dipack P Panjabi <dipack95@gmail.com>
2016-02-23 13:55:52 +0530

    Did some stuff

--- power-zcr-entropy-mfcc.py
+++ power-zcr-entropy-mfcc.py
@@ -9,19 +9,7 @@ from scipy import stats
 
 # np.set_printoptions(threshold=np.inf)
 
-targetPath = os.path.abspath("../sounds")
-
-zcrFiles = [os.path.join(path, name)
-             for path, dirs, files in os.walk(targetPath)
-             for name in files if ("angry" in name and name.endswith(("_zeroCrossing.csv")))]
-
-powerFiles = [os.path.join(path, name)
-             for path, dirs, files in os.walk(targetPath)
-             for name in files if ("angry" in name and name.endswith(("_powerSpectrum.csv")))]
-
-entropyFiles = [os.path.join(path, name)
-             for path, dirs, files in os.walk(targetPath)
-             for name in files if ("angry" in name and name.endswith(("_entropy.csv")))]
+# targetPath = os.path.abspath("../sounds/sounds/hack-the-talk-exotel-master/training_dataset")
 
 def splitSignal(localFile):
 
@@ -29,6 +17,7 @@ def splitSignal(localFile):
     mfcc = np.nan_to_num(np.array(pd.read_csv(localFile + '_mfcc.csv', header=None), dtype='float64'))
     zeroCrossing = np.nan_to_num(np.array(pd.read_csv(localFile + '_zeroCrossing.csv', header=None), dtype='float64'))
     entropy = np.nan_to_num(np.array(pd.read_csv(localFile + '_entropy.csv', header=None), dtype='float64'))
+    labels = np.array(pd.read_csv(localFile + '_labels.csv', header=None))
     zeroCrossing = np.ravel(zeroCrossing)
     entropy = np.ravel(entropy)
 
@@ -46,6 +35,10 @@ def splitSignal(localFile):
     blockOverlap = 250.0
     numOfJumps = np.ceil((lengthOfSignal * 1000.0) / blockOverlap)
 
+    if numOfJumps != labels.shape[0]:
+        print("Number of labels and number of jumps to make are not equal: Labels:", labels.shape[0], "Jumps:", numOfJumps)
+        return
+
     blockLength = blockDuration / overlapLength 
     startIndex = 0.0
     endIndex = blockLength
@@ -55,55 +48,22 @@ def splitSignal(localFile):
     frameEnd = blockLength
     frameOffset = np.floor(numOfFrames / numOfJumps)
 
-    # print(frameOffset, blockLength, numOfJumps, lengthOfSignal, numOfFrames)
+    print(frameOffset, blockLength, numOfJumps, lengthOfSignal, numOfFrames)
 
-    avgPower = np.mean(power, axis = 1)
-
-    print("Filename:", localFile.split('/')[-1].split('.wav')[0], "Length:", lengthOfSignal)
-
+    print("Filename:", localFile.split('/')[-1].split('.wav')[0], "Length:", lengthOfSignal, "Number of labels:", labels.shape, "Number of jumps:", numOfJumps)
     for i in range(0, int(numOfJumps)):
-        
-        tempPower = np.mean(avgPower[frameStart:frameEnd])
-        tempMfcc = mfcc[frameStart:frameEnd, ]
-        tempZcr = np.mean(zeroCrossing[frameStart:frameEnd])
-        tempEntropy = np.mean(entropy[frameStart:frameEnd])
-
-        ratio = []
-        for t in tempMfcc:
-            a, b, c, d, e, f = t[0], t[1], t[2], t[10], t[11], t[12]
-            av1 = sum(t[0:3]) / 3
-            av2 = sum(t[10:13]) / 3
-            ratio = np.append(ratio, av1 / av2)
-            
-        intraAverage = np.nan_to_num(np.array(ratio))
-        tempMfcc = np.mean(intraAverage)
-
-        zsPower = stats.zscore(avgPower[frameStart:frameEnd])
-        zsZcr = stats.zscore(zeroCrossing[frameStart:frameEnd])
-        zsEntropy = stats.zscore(entropy[frameStart:frameEnd])
-        zsMfcc = stats.zscore(intraAverage)
-
-        if not (np.isnan(tempPower)):
-            # Use the following print statement to print times, and labels
-            # print(startIndex * 0.01, "->", endIndex * 0.01, "Power:", tempPower)
-            print(startIndex *0.01, endIndex * 0.01)
-            # Use the following print statement to just print the values
-            # print(tempPower, round(tempMfcc, 7), tempEntropy)
-            # print(startIndex * 0.01, endIndex * 0.01, frameStart, frameEnd)
-            # print(avgPower[frameStart:frameEnd])
-            startIndex += durationOffset
-            endIndex += durationOffset
-            frameStart += frameOffset
-            frameEnd += frameOffset
-        else:
-            break
-    # print(avgPower)
+        print(startIndex * 0.01, "->", endIndex * 0.01, frameStart, frameEnd)
+        # Prints the first MFCC of each frame in the block, and prints its block label
+        for j in range(np.int(frameStart), np.int(frameEnd)):
+            print(j * 0.015, mfcc[j][0], labels[i])
+        startIndex += durationOffset
+        endIndex += durationOffset
+        frameStart += frameOffset
+        frameEnd += frameOffset
+    return
 
 def main():
-
-    # localFileName = '/home/dipack/College/Fourth_Year/Final_Year_Project/csv/training_dataset/angry/training_angry_10.wav'
-    # localFileName = '/home/dipack/College/Fourth_Year/Final_Year_Project/csv/Women/Pallavi/pallavi-normal.wav'
-    localFileName = '/home/dipack/College/Fourth_Year/Final_Year_Project/csv/Men/Tyrion/tyrion_neutral.wav'
+    localFileName = "/home/dipack/College/Fourth_Year/Final_Year_Project/csv/Men/Ajinkya/Ajinkya_angry.wav"
     splitSignal(localFileName)
 
 if __name__ == '__main__':
